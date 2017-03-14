eb6626ff4b34aa5c98b17116b0018bec549f8ca9
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2017-03-14 18:35:32 +0200

    Create SaveTable.py

---
+++ SaveTable.py
@@ -0,0 +1,28 @@
+from __future__ import print_function
+import mechanize
+import bs4 as bs
+import urllib2
+import codecs
+import sys
+import pandas as pd
+reload(sys)
+sys.setdefaultencoding('utf-8')
+URL = "http://sahibinden.com"
+search1=["villa","daire","mustakil"]
+search2=["kiralik","satilik"]
+b= mechanize.Browser()
+b.set_handle_refresh(False)
+b.addheaders=[('User-agent', "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")]
+for i in range(len(search1)):
+    for j in range(len(search2)):
+        b.open(URL, timeout=2.0)
+        b.select_form(nr=0)
+        b.form["query_text"]=str(search1[i])+" "+str(search2[j])
+        b.method="GET"
+        b.submit()
+        soup=bs.BeautifulSoup(b.open(b.geturl()).read(),"lxml")
+        dfs = pd.read_html(b.open(b.geturl()).read(),header=0)
+        for df in dfs:
+            df=str(df)
+            with open ("save.txt", 'a') as f:
+                f.write(df)
