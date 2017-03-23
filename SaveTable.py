25542dc389a78d313df8d98cfba72c2c00c89f3b
UtkucanBykl <utkucanbykl@gmail.com>
2017-03-24 01:25:26 +0200

    TDK Search

--- SaveTable.py
+++ SaveTable.py
@@ -1,4 +1,7 @@
 from __future__ import print_function
+
+import re
+
 import mechanize
 import bs4 as bs
 import urllib2
@@ -7,22 +10,19 @@ import sys
 import pandas as pd
 reload(sys)
 sys.setdefaultencoding('utf-8')
-URL = "http://sahibinden.com"
-search1=["villa","daire","mustakil"]
-search2=["kiralik","satilik"]
+kelime=raw_input("Aranan Kelime :")
+URL = "http://www.tdk.gov.tr/index.php?option=com_gts&view=gts"
 b= mechanize.Browser()
 b.set_handle_refresh(False)
 b.addheaders=[('User-agent', "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")]
-for i in range(len(search1)):
-    for j in range(len(search2)):
-        b.open(URL, timeout=2.0)
-        b.select_form(nr=0)
-        b.form["query_text"]=str(search1[i])+" "+str(search2[j])
-        b.method="GET"
-        b.submit()
-        soup=bs.BeautifulSoup(b.open(b.geturl()).read(),"lxml")
-        dfs = pd.read_html(b.open(b.geturl()).read(),header=0)
-        for df in dfs:
-            df=str(df)
-            with open ("save.txt", 'a') as f:
-                f.write(df)
+
+b.open(URL, timeout=2.0)
+b.select_form(nr=0)
+b.form["kelime"]=kelime
+b.method="post"
+response=b.submit()
+
+soup=bs.BeautifulSoup(response.read(),"lxml")
+for table in soup.find_all("table",id=re.compile("^hor-minimalist-a$")):
+    print (table.text)
+
