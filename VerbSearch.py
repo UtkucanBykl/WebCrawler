611e9f3398da2c1ba721b74ca0c3659ac26a80e4
Utkucan Bıyıklı <utkucanbykl@gmail.com>
2017-03-26 00:17:38 +0200

    Update VerbSearch.py

--- VerbSearch.py
+++ VerbSearch.py
@@ -1,28 +1,12 @@
-from __future__ import print_function
-
-import re
-
-import mechanize
-import bs4 as bs
-import urllib2
-import codecs
+import module
 import sys
-import pandas as pd
 reload(sys)
 sys.setdefaultencoding('utf-8')
 kelime=raw_input("Aranan Kelime :")
 URL = "http://www.tdk.gov.tr/index.php?option=com_gts&view=gts"
-b= mechanize.Browser()
-b.set_handle_refresh(False)
-b.addheaders=[('User-agent', "Mozilla/5.0 (Windows NT 6.1) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/41.0.2228.0 Safari/537.36")]
+b=module.WebCrawlerSearch(URL,"kelime",kelime)
+print (module.Crawle(b,"table","hor-minimalist-a"))
+print (module.FindLinks(URL))
 
-b.open(URL, timeout=2.0)
-b.select_form(nr=0)
-b.form["kelime"]=kelime
-b.method="post"
-response=b.submit()
 
-soup=bs.BeautifulSoup(response.read(),"lxml")
-for table in soup.find_all("table",id=re.compile("^hor-minimalist-a$")):
-    print (table.text)
 
